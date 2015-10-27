05b366910b98a8fba47aee026c4e6450b6ade1f9
GheloBear <ghelo.ace@gmail.com>
2015-10-27 19:22:51 +0800

    [ADD]  Initialized Module

---
+++ module/appartments/__openerp__.py
@@ -0,0 +1,13 @@
+# noinspection PyStatementEffect
+{
+    'name': "Apartment Management",
+    'version': "1.0",
+    'author': 'Angel Kenneth Tolentino',
+    'depends': [
+        'base'
+    ],
+    'description': "A module that allows users to check their Apartment Details",
+    'data': [
+
+    ],
+}
---
+++ module/appartments/models/__init__.py
@@ -0,0 +1,1 @@
+__author__ = 'GG-Vhin'
