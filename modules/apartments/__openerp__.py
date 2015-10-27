4d25e49ff4d3683366c3cb813d25f9ca89c5f75c
GheloBear <ghelo.ace@gmail.com>
2015-10-28 00:27:50 +0800

    [ADD]  Menu entry:  Leasers to list User whom have at least one apartment [ADD]  Generic Search View for search First, Last and Email in a single search [IMP]  Refactored Menu items to have a root and section entry [ADD]  Apartments may now be searched by Address

--- modules/apartments/__openerp__.py
+++ modules/apartments/__openerp__.py
@@ -5,12 +5,16 @@
     'author': 'Angel Kenneth Tolentino',
     'summary': "User may get and edit information about their apartments",
     'depends': [
-        'base'
+        'base',
+        'decimal_precision',
     ],
     'description': "A module that allows users to check their Apartment Details",
     'data': [
         'views/space_apartments_views.xml',
         'views/space_apartments_action.xml',
+        'views/space_apartments_search.xml',
         'views/res_users_views.xml',
+        'views/res_users_action.xml',
+        'views/res_users_search.xml',
     ],
 }
